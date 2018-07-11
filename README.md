41ac2082a9e0729f4d96151012431aae722071b9
Jesse Sadler <jsadler@ucla.edu>
2018-07-11 11:19:59 -0700

    Add README

---
+++ README.md
@@ -0,0 +1,7 @@
+# ICNS 2018: Visualizing Partible Inheritance among Early Modern Merchants
+
+This repository contains data, code, and plots used for my presentation on visualizing the inheritance of the Della Faille family from 1582 until 1594.
+
+The presentation was given at the International Conference of Netherlandic Studies on June 2nd, 2018 in Bloomington, Indiana.
+
+The plots serve as an example of types of visualization it is possible to make from an account book that uses double-entry bookkeeping. The analysis of the account book of the estate of Jan della Faille de Oude is done with my own [debkeepr package](https://github.com/jessesadler/debkeepr), which simplifies the analysis of accounting done the money of account of pounds, shillings, and pence.
--- scripts/arithmetic-demo.R
+++ scripts/arithmetic-demo.R
@@ -1,14 +1,18 @@
-library(tidyverse)
+## Demonstration of arithmetic with debkeepr ##
+
 library(debkeepr)
 
-# Multiplication: £15 3s. 8d. by 32
+# Normalization: £10 52s. 76d.
+deb_normalize(lsd = c(10, 52, 76))
 
+# Multiplication: £15 3s. 8d. by 32
+deb_multiply(lsd = c(15, 3, 8), x = 32)
 
 # Division: £465 12s. 8d. by 72
-
+deb_divide(lsd = c(465, 12, 8), x = 72)
 
 # 15783 pence to pounds, shillings, and pence
-
+deb_d_lsd(d = 15783)
 
 # Running account of Jacques and Daniel
 # 15 February 1585 to 23 May 1585
@@ -18,6 +22,8 @@ l <- c(26, 8, 5, 28, 4, 369, 85, 15, 2, 0, 155, 50, 120, 9, 2, 213, 71, 17, 93, 
 s <- c(4, 0, 1, 10, 7, 4, 4, 18, 18, 3, 1, 8, 11, 10, 9, 6, 5, 5, 3, 16)
 d <- c(0, 0, 10, 0, 8, 4, 11, 10, 6, 4, 4, 0, 0, 8, 3, 8, 0, 10, 1, 11)
 
-dvdm_57_85 <- tibble::tibble(l = l,
-                             s = s,
-                             d = d)
+dvdm_57_85 <- data.frame(l = l,
+                         s = s,
+                         d = d)
+
+deb_sum_df(dvdm_57_85, l = l, s = s, d = d)
